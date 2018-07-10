c5ea62e903d6b2153728a8535182563e185d2b54
improbable-valentyn <32096431+improbable-valentyn@users.noreply.github.com>
2018-07-10 03:40:16 -0700

    Export InitShadowData to enable RepNotify with old values (#9)

--- Engine/Source/Runtime/Engine/Public/Net/RepLayout.h
+++ Engine/Source/Runtime/Engine/Public/Net/RepLayout.h
@@ -467,10 +467,12 @@ public:
 
 	void OpenAcked( FRepState * RepState ) const;
 
-	void InitShadowData(
+	// IMPROBABLE-BEGIN - Added ENGINE_API to export this function
+	ENGINE_API void InitShadowData(
 		TArray< uint8, TAlignedHeapAllocator<16> >&	ShadowData,
 		UClass *									InObjectClass,
 		const uint8 * const							Src ) const;
+	// IMPROBABLE-END
 
 	void InitRepState( 
 		FRepState *									RepState, 
