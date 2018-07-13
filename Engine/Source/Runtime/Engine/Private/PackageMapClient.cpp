7b1d014cf202882b2e932aa4ca95a446e07c30e8
David Avedissian <david@improbable.io>
2018-07-13 12:27:02 +0100

    Disable pragma on PS4.

--- Engine/Source/Runtime/Engine/Private/PackageMapClient.cpp
+++ Engine/Source/Runtime/Engine/Private/PackageMapClient.cpp
@@ -1,6 +1,8 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 // IMPROBABLE-BEGIN - Turn off optimisations in this class
-#pragma optimize( "", off )  
+#ifndef PLATFORM_PS4
+#pragma optimize( "", off )
+#endif
 // IMPROBABLE-END
 
 #include "Engine/PackageMapClient.h"
@@ -3243,5 +3245,7 @@ FAutoConsoleCommand	ListNetGUIDExportsCommand(
 
 // ----------------------------------------------------------------
 // IMPROBABLE-BEGIN - Reset optimise pragma
+#ifndef PLATFORM_PS4
 #pragma optimize( "", on)
+#endif
 // IMPROBABLE-END
