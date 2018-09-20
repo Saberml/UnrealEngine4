8504d10e9d5fda17460f4c15c26fd93ebf9ba9d3
Sahil Dhanju <Vatyx@users.noreply.github.com>
2018-09-20 09:23:43 -0700

    Remove check for stably named path (#22)

--- Engine/Source/Runtime/Engine/Private/Level.cpp
+++ Engine/Source/Runtime/Engine/Private/Level.cpp
@@ -1763,7 +1763,7 @@ void ULevel::InitializeNetworkActors()
 				|| OwningWorld->WorldType == EWorldType::Game
 				|| OwningWorld->WorldType == EWorldType::GamePreview)
 			{
-				if (Actor->GetIsReplicated() && !Actor->IsPendingKill() && Actor->IsFullNameStableForNetworking())
+				if (Actor->GetIsReplicated() && !Actor->IsPendingKill())
 				{
 					Actor->Destroy(true);
 				}
